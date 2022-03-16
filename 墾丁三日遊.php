7ecfe9b7c06901acaf605111a15bbacc25fe3750
PengYuanCong <100682607+PengYuanCong@users.noreply.github.com>
2022-03-16 20:01:49 +0800

    Create 墾丁三日遊.php

---
+++ 墾丁三日遊.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>墾丁三日遊</title>
+<body>
+<form action="/墾丁三日遊.php" method="post">
+<p>1.行程內容</p>
+<p><input type="text" name="行程內容" value="行程內容"></p>
+
+<p>2.Name:</p><p><input type="text" name="name"></p>
+<p>email:</p><p><input type="text" name="email"></p>
+<p>telphone:</p><p><input type="text" name="telphone"></p>
+<p>Gender:</p><p><input type="radio" name="gender" value="男"></p><p><input type="radio" name="gender" value="女"></p>
+<p>Food Performance</p><p><input type="checkbox" name="food" value="烤肉"></p><p><input type="checkbox" name="food" value="火鍋"></p>
+<p>T-shirt Size</p>
+<select>
+<option>S</option>
+<option>M</option>
+<option>L</option>
+<option>XL</option>    
+</select>
+<p>T-shirt color</p><p><input type="text" name="color"></p>
+<p>Birthday</p><p><input type="text" name="birthday"></p>
+<p>Tickets</p><p><input type="text" name="tickets" value="number"></p>
+
+<p><input type="submit" value="submit"></p>
+
+</body>
+</head>
+</html>
