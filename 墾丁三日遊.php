dc49855fa7d63f060cdea93223aa74ab9ca15077
PengYuanCong <100682607+PengYuanCong@users.noreply.github.com>
2022-03-17 21:02:16 +0800

    Update 墾丁三日遊.php

--- 墾丁三日遊.php
+++ 墾丁三日遊.php
@@ -1,47 +1,56 @@
 <html>
 <head>
 <title>墾丁三日遊</title>
-<body>
-<form action="/墾丁三日遊.php" method="post">
-<p>1.行程內容</p>
-<textarea 
-name="行程內容"
-rows="2"
-cols="20"
-required>          
-</textarea>
-<textarea 
-name="行程內容"
-rows="2"
-cols="20"
-required>          
-</textarea>
+<body bgcolor="#FFF0F5">
+<form action="register.php" method="post">
+<table border="3" align="center" width="30%">
+<h1 align="center">墾丁三日遊報名資料</h1>
+<div><b><center><font size=6 color="#87CEFA">1.行程內容</font></b></div></center>
+<center><select name="route">
+    <option value="行程A;推薦年齡18~28歲">行程A;推薦年齡18~28歲</option>
+    <option value="行程B;推薦年齡29~40歲">行程B;推薦年齡29~40歲</option>
+    <option value="行程C;推薦年齡41~70歲">行程C;推薦年齡41~70歲</option>
+</select></center><br/>
 
-<p>2.Name: <input type="text" name="name"></p>
-<p>email:<input type="text" name="email"></p>
-<p>telphone:<input type="text" name="telphone"></p>
-<p>Gender:<input type="radio" name="gender" value="男">男 <input type="radio" name="gender" value="女">女</p>
-<p>Food Performance:<input type="checkbox" name="food" value="烤肉">烤肉 <input type="checkbox" name="food" value="火鍋">火鍋</p>
-<p>T-shirt Size:
-<select>
+<br/>
+<div><b><center><font size=6 color="#87CEFA">2.Name:<input type="text" name="name"></b><br/>
+<br/>
+<b>email:<input type="text" name="email"></b><br/>
+<br/>
+<b>telphone:<input type="text" name="telphone"></b><br/>
+<br/>
+<b>Gender:<input type="radio" name="gender" value="男"><font color="gray">男</font>
+          <input type="radio" name="gender" value="女"><font color="gray">女</font></b><br/>
+<br/>
+<b>Food Performance:<input type="checkbox" name="food[]" value="烤肉"><font color="red">烤肉</font>
+                    <input type="checkbox" name="food[]" value="火鍋"><font color="red">火鍋</font><br/>
+<br/>                    
+<b>T-shirt Size:
+<select name="size">
 <option>S</option>
 <option>M</option>
 <option>L</option>
 <option>XL</option>    
-</select>
+</select></b><br/>
+<br/>
+<b>T-shirt color:<input type="text" name="color"></b><br/>
+<br/>
+<b>Birthday:<input type="text" name="birthday"></b><br/>
+<br/>
+<b>Tickets:<input type="text" name="tickets" value="number"></b><br/>
+<br/>
+<b>T-shirt size 對照表</b><br/>
 
-<p>T-shirt color:<input type="text" name="color"></p>
-<p>Birthday:<input type="text" name="birthday"></p>
-<p>Tickets:<input type="text" name="tickets" value="number"></p>
-<p>T-shirt size 對照表</p>
-<table border="1">
     <tr><td>&nbsp&nbsp;</td><td>S</td><td>M</td><td>L</td><td>XL</td></tr>
     <tr><td>長</td><td>&nbsp&nbsp;</td><td>&nbsp&nbsp;</td><td>&nbsp&nbsp;</td><td>&nbsp&nbsp;</td></tr>
     <tr><td>寬</td><td>&nbsp&nbsp;</td><td>&nbsp&nbsp;</td><td>&nbsp&nbsp;</td><td>&nbsp&nbsp;</td></tr>
     <tr><td>高</td><td>&nbsp&nbsp;</td><td>&nbsp&nbsp;</td><td>&nbsp&nbsp;</td><td>&nbsp&nbsp;</td></tr>
-</table>
 
-<p><input type="submit" value="submit"></p>
+
+</table>
+<br/>
+<center><b><input type="submit" value="submit"></b></center>
+
 
 </body>
 </head>
